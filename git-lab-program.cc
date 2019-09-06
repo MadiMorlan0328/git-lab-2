897d9338cd8bac5ca47fb0dbdaafb0c100870b8b
Madison Morlan <mm299117@ohio.edu>
2019-09-06 14:38:37 -0400

    git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Madi Morlan
+ *        Date: 6 Sept 2019
+ * Description: Program for CSLab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    
+    return 0;
+}// main
